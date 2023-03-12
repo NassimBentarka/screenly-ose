1d678f1627deaec8ab9bf09c7fb24b7c0f9b7b6b
Nassim Bentarka <nassimbentarka@gmail.com>
2023-03-11 23:49:42 -0500

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -132,10 +132,10 @@ fi
 if [ -z "${REPOSITORY}" ]; then
   if [ "$WEB_UPGRADE" = false ]; then
     set -x
-    REPOSITORY=${1:-https://github.com/screenly/anthias.git}
+    REPOSITORY=${1:-https://github.com/NassimBentarka/screenly-ose.git}
   else
     set -e
-    REPOSITORY=https://github.com/screenly/anthias.git
+    REPOSITORY=https://github.com/NassimBentarka/screenly-ose.git
   fi
 fi
 
